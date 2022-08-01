0102d22bf20f3be7cfd9051bf6a6337e68b69151
Taufiqul Aptiyan Bagaskara <60858376+taufiqulaptiyan@users.noreply.github.com>
2022-08-01 11:43:39 +0700

    Add files via upload

---
+++ sur_easy_higer_value.js
@@ -0,0 +1,15 @@
+/**
+ * Direction:
+ * Find the higher value from the array bellow
+ *
+ * Expected Result:
+ * 8
+ */
+let numbers = [3, 1, 2, 3, 7, 5, 6, 8, 2, 1];
+
+function result(numbers) {
+  let max=Math.max.apply(Math,numbers);
+  return max;
+}
+
+console.log(result(numbers));
